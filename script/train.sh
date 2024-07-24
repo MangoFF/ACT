db74602c30c16a085521ab9dbee1386980bce68b
Mango <939117440@qq.com>
2024-07-24 21:51:50 +0800

    clear and clean

--- script/train.sh
+++ script/train.sh
@@ -2,30 +2,26 @@
 #SBATCH --job-name=multi_node_dpo
 #SBATCH --output=./output/multi_node_dpo.out
 #SBATCH --error=./output/multi_node_dpo.err
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1         # crucial - only 1 task per dist per node!
+#SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --gres=gpu:8
 #SBATCH --exclusive 
 #SBATCH --exclude g0066,g0036,g0067,g0029,g0021,g0017,g0011
-
-set -x -e
-
-which python
-
+#SBATCH --nodes=2
+echo "START TIME: $(date)"
 
 # 每个节点的显卡数
 GPUS_PER_NODE=8
 
 # 主节点和IP
 MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-MASTER_PORT=6005
+MASTER_PORT=6000
 
 NNODES=$SLURM_NNODES
 NODE_RANK=$SLURM_PROCID 
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-CMD="train.py \
-    --train_args_file dpo.json"
+CMD="act/train.py \
+    --train_args_file config/dpo.json"
 
 LAUNCHER="accelerate launch \
     --multi_gpu \
@@ -33,6 +29,7 @@ LAUNCHER="accelerate launch \
     --num_processes $WORLD_SIZE \
     --main_process_ip "$MASTER_ADDR" \
     --main_process_port $MASTER_PORT \
+    --num_processes $WORLD_SIZE \
     --machine_rank \$SLURM_PROCID \
     --role \$SLURMD_NODENAME: \
     --rdzv_conf rdzv_backend=c10d \
@@ -44,5 +41,8 @@ SRUN_ARGS=" \
     --wait=120 \
     --kill-on-bad-exit=1 \
 "
-srun bash -c "echo $LAUNCHER"
-srun bash -c "echo $CMD"
+
+clear; srun $SRUN_ARGS --jobid $SLURM_JOB_ID bash -c "$LAUNCHER $CMD" 
+
+echo "END TIME: $(date)"
+
